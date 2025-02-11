ad039294cd7e7d8bec74932741717092eb1ddc8a
eugenekim05 <eugenekim188@gmail.com>
2025-02-11 01:29:54 -0500

    added stuff

---
+++ studytool/src/app/Title.js
@@ -0,0 +1,11 @@
+// Title.js
+import React from 'react';
+import './Title.css'; // Importing the CSS file
+
+const Title = () => {
+    return (
+        <h1>Putt Project</h1>
+    );
+};
+
+export default Title;
